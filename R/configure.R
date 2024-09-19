9fa9e6451e54d8d6420fbe6f9566da666e162ba7
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-09-19 12:29:10 -0700

    Fleshed out configure() to control which settings are propagated.
    Also added a dedicated formatArguments() to convert Cmake options in R
    to appropriate command-line arguments.

--- R/configure.R
+++ R/configure.R
@@ -2,20 +2,29 @@
 #'
 #' Propagate R's configuration variables into the Cmake options, where possible.
 #'
-#' @param compact Logical scalar indicating whether to format the options as Cmake command-line arguments.
+#' @param c.compiler Logical scalar indicating whether to propagate R's choice of C compiler.
+#' @param cxx.compiler Logical scalar indicating whether to propagate R's choice of C++ compiler.
+#' @param fortran.compiler Logical scalar indicating whether to propagate R's choice of Fortran compiler.
+#' @param make Logical scalar indicating whether to propagate R's choice of \code{make} command.
+#' @param ar Logical scalar indicating whether to propagate R's choice of command to make static libraries.
+#' @param ranlib Logical scalar indicating whether to propagate R's choice of command to index static libraries.
+#' @param options Character vector of optional arguments from \code{configure}.
 #'
 #' @return
-#' If \code{compact}, a character vector containing CMake command-line arguments.
+#' For \code{configure}, a named character vector containing the name and value of each option.
 #'
-#' Otherwise, a named character vector containing the name and value of each option.
+#' For \code{formatArguments}, a character vector with Cmake arguments on the command line.
+#' \code{NA} values are ignored, and values with spaces or empty strings are quoted.
 #'
 #' @author Aaron Lun
 #'
 #' @examples
-#' configure()
+#' options <- configure()
+#' options
+#' formatArguments(options)
 #'
 #' @export
-configure <- function(compact=TRUE) {
+configure <- function(c.compiler=TRUE, cxx.compiler=TRUE, fortran.compiler=TRUE, make=TRUE, ar=TRUE, ranlib=TRUE) {
     options <- list()
 
     options[["CMAKE_POSITION_INDEPENDENT_CODE"]] <- "ON"
@@ -25,47 +34,61 @@ configure <- function(compact=TRUE) {
     }
 
     if (Sys.info()[["sysname"]] == "Darwin") {
-         options[["CMAKE_OSX_DEPLOYMENT_TARGET"]] <- "\"\"" # avoiding hard-coding of exact OSX version.
+         options[["CMAKE_OSX_DEPLOYMENT_TARGET"]] <- "" # avoiding hard-coding of exact OSX version.
     }
 
     r.self <- file.path(R.home("bin"), "R")
 
-    c_compiler <- sub(" .*", "", system2(r.self, c("CMD", "config", "CC"), stdout=TRUE))
-    if (Sys.which(c_compiler) != "" || file.exists(c_compiler)) {
-        options[["CMAKE_C_COMPILER"]] <- c_compiler
+    if (c.compiler) {
+        out <- sub(" .*", "", system2(r.self, c("CMD", "config", "CC"), stdout=TRUE))
+        if (Sys.which(out) != "" || file.exists(out)) {
+            options[["CMAKE_C_COMPILER"]] <- out
+        }
     }
 
-    cxx_compiler <- sub(" .*", "", system2(r.self, c("CMD", "config", "CXX"), stdout=TRUE))
-    if (Sys.which(cxx_compiler) != "" || file.exists(cxx_compiler)) {
-        options[["CMAKE_CXX_COMPILER"]] <- cxx_compiler
+    if (cxx.compiler) {
+        out <- sub(" .*", "", system2(r.self, c("CMD", "config", "CXX"), stdout=TRUE))
+        if (Sys.which(out) != "" || file.exists(out)) {
+            options[["CMAKE_CXX_COMPILER"]] <- out
+        }
     }
 
-    f_compiler <- sub(" .*", "", system2(r.self, c("CMD", "config", "FC"), stdout=TRUE))
-    if (Sys.which(f_compiler) != "" || file.exists(f_compiler)) {
-        options[["CMAKE_FORTRAN_COMPILER"]] <- f_compiler
+    if (fortran.compiler) {
+        out <- sub(" .*", "", system2(r.self, c("CMD", "config", "FC"), stdout=TRUE))
+        if (Sys.which(out) != "" || file.exists(out)) {
+            options[["CMAKE_FORTRAN_COMPILER"]] <- out
+        }
+    }
+ 
+    if (make) {
+        out <- sub(" .*", "", system2(r.self, c("CMD", "config", "MAKE"), stdout=TRUE))
+        if (Sys.which(out) != "" || file.exists(out)) {
+            options[["CMAKE_MAKE_PROGRAM"]] <- out
+        }
     }
 
-    make <- sub(" .*", "", system2(r.self, c("CMD", "config", "MAKE"), stdout=TRUE))
-    if (Sys.which(make) != "" || file.exists(make)) {
-        options[["CMAKE_MAKE_PROGRAM"]] <- make
+    if (ar) {
+        out <- sub(" .*", "", system2(r.self, c("CMD", "config", "AR"), stdout=TRUE))
+        if (Sys.which(out) != "" || file.exists(out)) {
+            options[["CMAKE_AR"]] <- out
+        }
     }
 
-    ar <- sub(" .*", "", system2(r.self, c("CMD", "config", "AR"), stdout=TRUE))
-    if (Sys.which(make) != "" || file.exists(make)) {
-        options[["CMAKE_AR"]] <- ar
+    if (ranlib) {
+        out <- sub(" .*", "", system2(r.self, c("CMD", "config", "RANLIB"), stdout=TRUE))
+        if (Sys.which(out) != "" || file.exists(out)) {
+            options[["CMAKE_RANLIB"]] <- out
+        }
     }
 
-    ranlib <- sub(" .*", "", system2(r.self, c("CMD", "config", "RANLIB"), stdout=TRUE))
-    if (Sys.which(ranlib) != "" || file.exists(ranlib)) {
-        options[["CMAKE_RANLIB"]] <- ranlib
-    }
+    unlist(options)
+}
 
-    options <- unlist(options)
-    if (compact) {
-        needs.quote <- grep(" ", options)
-        options[needs.quote] <- shQuote(options[needs.quote])
-        options <- sprintf("-D%s=%s", names(options), options)
-    }
-
-    options
+#' @export
+#' @rdname configure
+formatArguments <- function(options) {
+    options <- options[!is.na(options)]
+    needs.quote <- grepl(" ", options) | options == ""
+    options[needs.quote] <- shQuote(options[needs.quote])
+    options <- sprintf("-D%s=%s", names(options), options)
 }
